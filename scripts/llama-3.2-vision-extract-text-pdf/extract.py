97681b8f67356ac064848d63cb842d30cfb41cb1
Danny Willems <be.danny.willems@gmail.com>
2024-12-31 17:10:37 +0100

    Update

--- scripts/llama-3.2-vision-extract-text-pdf/extract.py
+++ scripts/llama-3.2-vision-extract-text-pdf/extract.py
@@ -11,20 +11,20 @@ filename = sys.argv[1]
 # We always want the same output.
 temperature=0.0
 
-print("Processing %s" % img)
-response = chat(model='llama3.2-vision', messages=[
-  {
-    'role': 'user',
-    'content': 'Here is a page of a research paper in cryptography, given as an image extracted from the original PDF. Extract the content into MarkDown, and extract in LaTeX for the equations and mathematical symbols. Keep the exact same content. Do not describe what is in the paper. The paper is splitted in different images, therefore do not add any section.',
-    'images': [img],
-    'temperature': temperature,
-  }],
-)
+print("Processing %s" % filename)
+response = chat(
+        model='llama3.2-vision',
+        messages=[{
+            'role': 'user',
+            'content': 'Here is a page of a research paper in cryptography, given as an image extracted from the original PDF. Extract the content into MarkDown using LaTeX for the equations and mathematical symbols. Keep the exact same content. Do not describe what is in the paper. The paper is splitted in different images, therefore do not add any section.',
+            'images': [filename],
+            'temperature': temperature,
+        }],
+    )
 
-print(response['message']['content'])
 # or access fields directly from the response object
 print(response.message.content)
-md_filename = "%s.md" % img.stem
+md_filename = "%s.md" % filename.stem
 with open(md_filename, "w") as f:
     print("Writing result in %s" % md_filename)
     f.write(response.message.content)
